497ff26819e931d66ae3eae6653d6197e245877a
Ramzel Tan <ramzeltan@outlook.com>
2025-07-04 17:10:38 +0800

    Initialization

---
+++ src/utils/gsap.js
@@ -0,0 +1,7 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+export { gsap, ScrollTrigger };
